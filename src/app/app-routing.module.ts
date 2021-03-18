27e6059a5281cccb4c15956516c49bad3b49bd36
ari89zSD <arifzsidd@gmail.com>
2021-03-18 02:51:01 -0400

    brought everything up-to-par

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MarketComponent } from './portal/market/market.component';
+import { WelcomeComponent } from './portal/welcome/welcome.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: WelcomeComponent },
+  { path: 'market', component: MarketComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MarketComponent } from './portal/market/market.component';
+import { WelcomeComponent } from './portal/welcome/welcome.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MarketComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
