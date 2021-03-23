71ba4d76d360201edf85049b23c0db4772baf9f8
ari89zSD <arifzsidd@gmail.com>
2021-03-23 00:47:23 -0400

    added typesense client library for integration with typesense search, changed mappings to scripts and images

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
 
+declare var Typesense: any;
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+  client = new Typesense.Client({
+    'nodes': [{
+      'host': 'localhost',
+      'port': '8108',
+      'protocol': 'http'
+    }],
+    'apiKey': 'Rhsdhas2asasdasj2',
+    'connectionTimeoutSeconds': 2
+  });
+
   title = 'mrkz04';
 }
