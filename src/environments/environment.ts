a234c39e642d1f3c41d19c48018d8fb32ac5aa32
ari89zSD <arifzsidd@gmail.com>
2021-04-08 00:57:22 -0400

    Revert "Integrated typesense search adapter, instantsearch"
    This reverts commit 78ca746f1b84d1795fc283f448c6318657fb504e.
    
    "reverting back to previous state before service"

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,15 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  typesenseServer: {
-    apiKey: 'Rhsdhas2asasdasj2', // Be sure to use the search-only-api-key
-    node1: {
-      host: 'localhost',
-      port: '8108',
-      protocol: 'http',
-    },
-  },
+  production: false
 };
 
 /*
